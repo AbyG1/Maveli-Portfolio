5b1b1a70d8c2a2092ee4ec8e8896b73d7d6e7857
Aby George <abygeorge133@gmail.com>
2023-09-03 22:52:34 +0530

    Set about me

--- index.js
+++ index.js
@@ -2,13 +2,12 @@ window.onscroll = function() {
     myFunction();
   };
   
-  // Get the navbar
+  
   const navbar = document.querySelector(".navbar");
   
-  // Get the offset position of the navbar
   const sticky = navbar.offsetTop;
   
-  // Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+
   function myFunction() {
     if (document.documentElement.scrollTop >= sticky) {
       navbar.classList.add("sticky");
